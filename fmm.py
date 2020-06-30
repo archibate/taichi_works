facc91cf740b631f28e46aead57b177c96a9d238
archibate <1931127624@qq.com>
2020-06-30 14:26:59 +0800

    build tree

--- fmm.py
+++ fmm.py
@@ -3,11 +3,31 @@ import taichi_glsl as ts
 from taichi_glsl import vec, vec2
 
 
+class RawGrader(ts.DataOriented):
+    @ti.func
+    def compute_grad(self, pos):
+        acc = vec2(0.0)
+        for i in range(self.N):
+            dir = self.pos[i] - pos
+            dir = ts.normalizePow(dir, -2, 1e-3)
+            acc += dir
+        return acc
+
+
 class FMMGrader(ts.DataOriented):
-    def fmm_init(self, L=4):
+    def fmm_init(self, L=4, mps=4):
+        self.level = L
+        self.n_grid = 2 ** L
+        self.grid_size = 1 / self.n_grid
+        self.max_per_cell = mps
+        # Level 0 is the minimal cells.
+        # Level L-1 is four maxmimum cells.
+        # Level L is the root cell.
         self.g_id = ti.var(ti.i32)
-        self.tree = ti.root.bitmasked(ti.ij, 2 ** (L + 1))
-        self.tree.place(self.g_id)
+        self.tree = ti.root.bitmasked(ti.ij, self.n_grid)
+        self.tree_ps = self.tree.dynamic(ti.k, self.max_per_cell)
+        self.tree_ps.place(self.g_id)
+        self.img = ti.var(ti.f32, (512, 512))
 
     @staticmethod
     @ti.func
@@ -26,8 +46,25 @@ class FMMGrader(ts.DataOriented):
 
     @ti.func
     def build_tree(self):
+        for I in ti.grouped(self.tree):
+            ti.deactivate(self.tree_ps, I)
         for i in range(self.N):
-            self.g_id[P] = i
+            P = int(self.pos[i] * self.n_grid)
+            ti.append(self.tree_ps, P, i)
+
+    @ti.func
+    def paint_tree(self):
+        for I in ti.grouped(self.img):
+            self.img[I] = 0
+        for I in ti.grouped(self.tree):
+            gs = int(self.grid_size * 512)
+            P = int(I * gs)
+            for off in ti.grouped(ti.ndrange(gs, gs)):
+                self.img[P.xy + off] = ti.length(self.tree_ps, I) / self.max_per_cell
+
+    @ti.func
+    def cell_id(self, level, offset):
+        2 ** level
 
     @ti.func
     def compute_grad(self, pos):
@@ -61,11 +98,16 @@ class MySolver(ts.Animation, FMMGrader):
 
     @ti.kernel
     def on_advance(self):
+        self.build_tree()
         for i in self.pos:
             acc = self.compute_grad(self.pos[i])
             self.vel[i] += acc * self.dt
         for i in self.pos:
             self.pos[i] += self.vel[i] * self.dt
+
+    @ti.kernel
+    def on_render(self):
+        self.paint_tree()
 
 
 
