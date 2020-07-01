8ce24b1fdb3e6a86d2f57e8fb5e6d815e0ae3d69
archibate <1931127624@qq.com>
2020-07-01 17:21:37 +0800

    prewe

--- wet.py
+++ wet.py
@@ -9,9 +9,13 @@ class Particle(tl.TaichiClass):
     def pos(self):
         return self.entries[0]
 
+    @property
+    def vel(self):
+        return self.entries[1]
+
     @classmethod
     def _var(cls, *_, **__):
-        return ti.Vector(2, ti.f32, *_, **__)
+        return ti.Vector(2, ti.f32, *_, **__), ti.Vector(2, ti.f32, *_, **__)
 
 
 class Node(tl.TaichiClass):
@@ -30,7 +34,8 @@ class Node(tl.TaichiClass):
 
 
 ## Define Variables
-N = 1
+dt = 0.0001
+N = 6
 L = 4
 RES = 512
 particles = Particle.var(N)
@@ -85,7 +90,7 @@ def npow(x):
 ## Main Program
 @ti.kernel
 def init():
-    particles.pos[0] = tl.randND(2)
+    #particles.pos[0] = tl.randND(2)
     if ~int(particles.pos[0].x * 9999) != -33:
         for i in range(N):
             particles.pos[i] = tl.randND(2)
@@ -101,7 +106,7 @@ def old_compute_grad(p):
 def compute_grad(p):
     acc = p * 0
 
-    print('\033[H\033[2J\033[3J')
+    #print('\033[H\033[2J\033[3J')
     for ch in range(4):
         i = 4 + ch
         if not ti.is_active(tree, i):
@@ -113,9 +118,9 @@ def compute_grad(p):
             #print('l1', ch)
             acc += npow(np2p) * nodes[i].mass
         else:
-            bas = (ch % 2) * 4 + (ch // 2) * 8
+            bas = (ch % 2) * 2 + (ch // 2) * 8
             for ch_ in range(4):
-                i = 16 + bas + ch_
+                i = 16 + bas + (ch_ % 2) + (ch_ // 2) * 4
                 if not ti.is_active(tree, i):
                     continue
                 size_sqr = 1 / 16
@@ -128,7 +133,7 @@ def compute_grad(p):
                     baso = (ch % 2) * 4 + (ch // 2) * 8 * 4
                     bas_ = (ch_ % 2) * 2 + (ch_ // 2) * 8 * 2
                     for ch__ in range(4):
-                        i = 64 + baso + bas_ + ch__
+                        i = 64 + baso + bas_ + (ch__ % 2) + (ch__ // 2) * 8
                         if not ti.is_active(tree, i):
                             continue
                         size_sqr = 1 / 64
@@ -142,9 +147,30 @@ def compute_grad(p):
 @ti.kernel
 def render(mx: ti.f32, my: ti.f32):
     p = vec(mx, my)
-    acc = compute_grad(p) * 0.008
+    acc = compute_grad(p) * 0.001
     tl.paintArrow(image, p, acc)
     paint_tree()
+
+@ti.kernel
+def advance():
+    for i in range(N):
+        acc = compute_grad(particles.pos[i]) * 0.01
+        particles.vel[i] += acc * dt
+    for i in range(N):
+        particles.pos[i] += particles.vel[i] * dt
+        particles.vel[i] = tl.boundReflect(particles.pos[i], particles.vel[i])
+
+
+@ti.kernel
+def compute_energy():
+    kine_eng = 0.0
+    pote_eng = 0.0
+    for i in range(N):
+        kine_eng += tl.sqrLength(particles.vel[i])
+    for i in range(N):
+        for j in range(i):
+            pote_eng += tl.invLength(particles.pos[i] - particles.pos[j])
+    print(kine_eng * 0.5 - pote_eng * 0.01)
 
 
 ## GUI Loop
@@ -152,11 +178,14 @@ init()
 with ti.GUI('Tree-code Gravity', RES) as gui:
     while gui.running and not gui.get_event(gui.ESCAPE):
         image.fill(0)
-        nodes.mpos.fill(0)
-        nodes.mass.fill(0)
-        tree.deactivate_all()
-        build_tree()
-        render(*gui.get_cursor_pos())
+        for i in range(50):
+            nodes.mpos.fill(0)
+            nodes.mass.fill(0)
+            tree.deactivate_all()
+            build_tree()
+            advance()
+        compute_energy()
+        #render(*gui.get_cursor_pos())
         gui.set_image(image)
         gui.circles(particles.pos.to_numpy(), radius=2)
         gui.show()
