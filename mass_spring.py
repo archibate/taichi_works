69b076e6cc88860e769dd63696c551f6d79a8415
archibate <1931127624@qq.com>
2020-07-03 01:02:12 +0800

    3d

--- mass_spring.py
+++ mass_spring.py
@@ -14,19 +14,19 @@ L = W / N
 beta = 0.5
 beta_dt = beta * dt
 alpha_dt = (1 - beta) * dt
-stiff = 2000
+stiff = 3000
 
 
-x = ti.Vector(2, ti.f32, NN)
-v = ti.Vector(2, ti.f32, NN)
-b = ti.Vector(2, ti.f32, NN)
-F = ti.Vector(2, ti.f32, NN)
+x = ti.Vector(3, ti.f32, NN)
+v = ti.Vector(3, ti.f32, NN)
+b = ti.Vector(3, ti.f32, NN)
+F = ti.Vector(3, ti.f32, NN)
 
 
 @ti.kernel
 def init():
-    for i in ti.grouped(ti.ndrange(N, N)):
-        x[i] = (i + 0.5) * L - 0.5
+    for i in ti.grouped(x):
+        x[i] = tl.vec((i + 0.5) * L - 0.5, 0)
 
 @ti.kernel
 def explicit():
@@ -36,7 +36,7 @@ def explicit():
     '''
     for i, j in K:
         disp = x[j] - x[i]
-        acc = K[i, j] * disp * (disp.norm() - L) / L ** 2
+        acc = K[i, j] * disp * (disp.norm() - L) / L**2
         v[i] += dt * acc
     for i in x:
         x[i] += dt * v[i]
@@ -57,10 +57,10 @@ links = [tl.vec(*_) for _ in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
 @ti.func
 def Acc(v: ti.template(), x: ti.template(), dt):
     for i in ti.grouped(x):
-        acc = tl.vec2(0.0)
+        acc = x[i] * 0
         for d in ti.static(links):
             disp = x[tl.clamp(i + d, 0, tl.vec(*NN) - 1)] - x[i]
-            acc += disp * (disp.norm() - L) / L ** 2
+            acc += disp * (disp.norm() - L) / L**2
         v[i] += stiff * acc * dt
 
 @ti.kernel
@@ -75,8 +75,8 @@ def prepare():
 @ti.kernel
 def jacobi():
     for i in ti.grouped(x):
-        b[i] = x[i] + F[i] * beta_dt ** 2
-        F[i] = tl.vec2(0)
+        b[i] = x[i] + F[i] * beta_dt**2
+        F[i] = b[i] * 0
     Acc(F, b, 1)
 
 @ti.kernel
@@ -99,31 +99,52 @@ def implicit():
     update_pos()
     collide()
 
+
+scene = t3.Scene()
+model = t3.Model()
+scene.add_model(model)
+
+faces = t3.Face.var(N**2 * 3)
+lines = t3.Line.var(N**2 * 2)
+vertices = t3.Vertex.var(N**2)
+model.set_vertices(vertices)
+model.add_geometry(faces)
+model.add_geometry(lines)
+
 @ti.kernel
-def export_lines(out: ti.ext_arr()) -> ti.i32:
-    n = 0
-    for i, j in K:
-        if i < j:
-            p = ti.atomic_add(n, 1)
-            out[p, 0] = i
-            out[p, 1] = j
-    return n
+def init_display():
+    for i_ in ti.grouped(ti.ndrange(N - 1, N - 1)):
+        i = i_
+        a = i.dot(tl.vec(N, 1))
+        i.x += 1
+        b = i.dot(tl.vec(N, 1))
+        i.y += 1
+        c = i.dot(tl.vec(N, 1))
+        i.x -= 1
+        d = i.dot(tl.vec(N, 1))
+        i.y -= 1
+        faces[a * 2 + 0].idx = tl.vec(a, b, c)
+        faces[a * 2 + 1].idx = tl.vec(a, c, d)
+        lines[a * 2 + 0].idx = tl.vec(a, b)
+        lines[a * 2 + 1].idx = tl.vec(a, d)
+
+@ti.kernel
+def update_display():
+    for i in ti.grouped(x):
+        vertices[i.dot(tl.vec(N, 1))].pos = x[i]
 
 
 init()
+init_display()
+scene.set_light_dir([-1, 1, -1])
 with ti.GUI('Mass Spring') as gui:
     while gui.running and not gui.get_event(gui.ESCAPE):
         if not gui.is_pressed(gui.SPACE):
-            implicit()
+            for i in range(1):
+                implicit()
+            update_display()
 
-        x_ = x.to_numpy().reshape(N * N, 2) * 0.5 + 0.5
-
-        if 0:
-            kar = np.empty((N * 2, 2), np.int32)
-            kar_n = export_lines(kar)
-            for i in range(kar_n):
-                a, b = kar[i]
-                gui.line(x_[a], x_[b])
-
-        gui.circles(x_, radius=2)
+        scene.camera.from_mouse(gui)
+        scene.render()
+        gui.set_image(scene.img)
         gui.show()
