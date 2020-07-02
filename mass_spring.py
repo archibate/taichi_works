a9501fa50c8d7458a1ee22ff52c74949c9e81978
archibate <1931127624@qq.com>
2020-07-03 00:23:08 +0800

    wel

--- mass_spring.py
+++ mass_spring.py
@@ -5,41 +5,27 @@ import math
 ti.init()
 
 
-dt = 0.01
-n = 16
-N = n ** 2
+dt = 0.003
+N = 32
+NN = N, N
 W = 1
-L = W / n
+L = W / N
 beta = 0.5
 beta_dt = beta * dt
 alpha_dt = (1 - beta) * dt
-stiff = 100
+stiff = 2000
 
 
-x = ti.Vector(2, ti.f32, N)
-v = ti.Vector(2, ti.f32, N)
-b = ti.Vector(2, ti.f32, N)
-F = ti.Vector(2, ti.f32, N)
-K = ti.var(ti.f32)
-ti.root.bitmasked(ti.ij, N).place(K)
+x = ti.Vector(2, ti.f32, NN)
+v = ti.Vector(2, ti.f32, NN)
+b = ti.Vector(2, ti.f32, NN)
+F = ti.Vector(2, ti.f32, NN)
 
-
-@ti.func
-def link(i, j, k):
-    K[j, i] = k
-    K[i, j] = k
 
 @ti.kernel
 def init():
-    for i, j in ti.ndrange(n, n):
-        a = i * n + j
-        x[a] = ((tl.vec(i, j) + 0.5) * L - 0.5)
-    for i, j in ti.ndrange(n - 1, n - 1):
-        a = i * n + j
-        link(a, a + n, stiff)
-        link(a + n, a + n + 1, stiff)
-        link(a + n + 1, a + 1, stiff)
-        link(a + 1, a, stiff)
+    for i in ti.grouped(ti.ndrange(N, N)):
+        x[i] = (i + 0.5) * L - 0.5
 
 @ti.kernel
 def explicit():
@@ -49,8 +35,7 @@ def explicit():
     '''
     for i, j in K:
         disp = x[j] - x[i]
-        d = disp.norm()
-        acc = K[i, j] * disp * (d - L) / L ** 2
+        acc = K[i, j] * disp * (disp.norm() - L) / L ** 2
         v[i] += dt * acc
     for i in x:
         x[i] += dt * v[i]
@@ -66,39 +51,43 @@ v' = v + Mdt @ [beta v' + alpha v]
 (I - beta Mdt) @ v' = (I + alpha Mdt) @ v
 '''
 
+links = [tl.vec(*_) for _ in [(-1, 0), (1, 0), (0, -1), (0, 1)]]
+
 @ti.func
 def Acc(v: ti.template(), x: ti.template(), dt):
-    for i, j in K:
-        disp = x[j] - x[i]
-        acc = K[i, j] * disp * (disp.norm() - L) / L ** 2
-        v[i] += acc * dt
+    for i in ti.grouped(x):
+        acc = tl.vec2(0.0)
+        for d in ti.static(links):
+            disp = x[tl.clamp(i + d, 0, tl.vec(*NN) - 1)] - x[i]
+            acc += disp * (disp.norm() - L) / L ** 2
+        v[i] += stiff * acc * dt
 
 @ti.kernel
 def prepare():
-    for i in x:
+    for i in ti.grouped(x):
         x[i] += v[i] * alpha_dt
     Acc(v, x, alpha_dt)
-    for i in x:
+    for i in ti.grouped(x):
         b[i] = x[i]
         x[i] += v[i] * beta_dt
 
 @ti.kernel
 def jacobi():
-    for i in v:
+    for i in ti.grouped(x):
         b[i] = x[i] + F[i] * beta_dt ** 2
-        F[i] = [0.0, 0.0]
+        F[i] = tl.vec2(0)
     Acc(F, b, 1)
 
 @ti.kernel
 def collide():
-    for i in x:
-        v[i].y -= 0.01 * dt
-    for i in x:
+    for i in ti.grouped(x):
+        v[i].y -= 0.98 * dt
+    for i in ti.grouped(x):
         v[i] = tl.boundReflect(b[i], v[i], -1, 1)
 
 @ti.kernel
 def update_pos():
-    for i in x:
+    for i in ti.grouped(x):
         x[i] = b[i]
         v[i] += F[i] * beta_dt
 
@@ -126,9 +115,9 @@ with ti.GUI('Mass Spring') as gui:
         if not gui.is_pressed(gui.SPACE):
             implicit()
 
-        x_ = x.to_numpy() * 0.5 + 0.5
+        x_ = x.to_numpy().reshape(N * N, 2) * 0.5 + 0.5
 
-        if 1:
+        if 0:
             kar = np.empty((N * 2, 2), np.int32)
             kar_n = export_lines(kar)
             for i in range(kar_n):
