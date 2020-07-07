48742ac651318827b2e99e2e0ba1d181b20d786d
archibate <1931127624@qq.com>
2020-07-07 15:59:45 +0800

    try stride:

--- cyclic.py
+++ cyclic.py
@@ -5,6 +5,9 @@ N = 8
 
 x = ti.var(ti.f32, N)
 m = ti.var(ti.f32, (N, N))
+r = ti.var(ti.f32, (N, N))
+ma = ti.var(ti.f32, N)
+mc = ti.var(ti.f32, N)
 
 
 @ti.kernel
@@ -15,7 +18,7 @@ def init():
         m[(i + 1) % N, i] = -1
 
 
-def show():
+def show(m):
     for i in range(N):
         for j in range(N):
             if m[i, j] != 0:
@@ -39,21 +42,28 @@ def cyclic():
 
 
 @ti.kernel
-def cyclic():
-    i = 2
+def cyclic(i: ti.i32, s: ti.i32, o: ti.i32, n: ti.template()):
     a = i
     b, c = (i + tl.vec(1, 2)) % N
-    k = -m[b, a] / m[a, a]
-    for t in ti.static(range(-2, 3)):
-        m[a, a + t] *= k
-    k = -m[b, c] / m[c, c]
-    for t in ti.static(range(-2, 3)):
-        m[c, c + t] *= k
+    k = -m[s * b + o, s * a + o] / m[s * a + o, s * a + o]
+    for t in ti.static(range(n)):
+        ma[t] = m[a, t] * k
+    k = -m[s * b + o, s * c + o] / m[s * c + o, s * c + o]
+    for t in ti.static(range(n)):
+        mc[t] = m[s * c + o, t] * k
 
-    for t in ti.static(range(-2, 3)):
-        m[b, b + t] += m[a, b + t] + m[c, b + t]
+    for t in ti.static(range(n)):
+        r[s * b + o, s * t + o] = m[s * b + o, s * t + o] + ma[t] + mc[t]
 
 
 init()
-cyclic()
-show()
+for i in range(N):
+    cyclic(i, 1, 0, N)
+m.from_numpy(r.to_numpy())
+show(m)
+print('=========')
+for i in range(N // 2):
+    for j in range(2):
+        cyclic(i, 2, j, N // 2)
+m.from_numpy(r.to_numpy())
+show(m)
