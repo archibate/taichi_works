94973dcf6195ff5bfdbeb6900ea2c08020dab270
archibate <1931127624@qq.com>
2020-07-17 15:39:46 +0800

    update_P

--- fem.py
+++ fem.py
@@ -6,12 +6,18 @@ NF = 6
 NV = NF + 2
 E, nu = 5e3, 0.2
 mu, lam = E / 2 / (1 + nu), E * nu / (1 + nu) / (1 - 2 * nu)
+damping = 100.0
+dt = 0.001
 
 vertices = ti.Vector.var(2, ti.f32, NV)
+velocity = ti.Vector.var(2, ti.f32, NV)
 faces = ti.Vector.var(3, ti.i32, NF)
 B = ti.Matrix.var(2, 2, ti.f32, NF)
 F = ti.Matrix.var(2, 2, ti.f32, NF)
+dF = ti.Vector.var(2, ti.f32, (NF, NV))
+P = ti.Matrix.var(2, 2, ti.f32, NF)
 phi = ti.var(ti.f32, NF)
+V = ti.var(ti.f32, NF)
 
 
 @ti.kernel
@@ -26,15 +32,42 @@ def update_B():
 @ti.kernel
 def update_F():
     for i in range(NF):
-        a = vertices[faces[i].x]
-        b = vertices[faces[i].y]
-        c = vertices[faces[i].z]
+        ia, ib, ic = faces[i]
+        a = vertices[ia]
+        b = vertices[ib]
+        c = vertices[ic]
         D_i = ti.Matrix.cols([a - c, b - c])
         F[i] = D_i @ B[i]
+        dF[i, ia] += B[i] @ ti.Vector([1, 0])
+        dF[i, ib] += B[i] @ ti.Vector([0, 1])
+        dF[i, ic] += B[i] @ ti.Vector([-1, -1])
+        V[i] = abs((a - c).cross(b - c))
 
 
 @ti.kernel
 def update_P():
+    for i in range(NF):
+        F_i = F[i]
+        J_i = F_i.determinant()
+        log_J_i = ti.log(J_i)
+        P_i = mu * (F_i - F_i.transpose())
+        P[i] = P_i
+
+
+@ti.kernel
+def advance():
+    for i in range(NV):
+        f_i = tl.vec2(0.0)
+        for e in range(NF):
+            f_i += V[e] * P[e] @ dF[e, i]
+        velocity[i] += -dt * f_i
+        velocity[i] *= ti.exp(-dt * damping)
+    for i in range(NV):
+        vertices[i] += dt * velocity[i]
+
+
+@ti.kernel
+def update_phi():
     for i in range(NF):
         F_i = F[i]
         J_i = F_i.determinant()
@@ -45,12 +78,12 @@ def update_P():
         phi[i] = phi_i
 
 
-def paint_P():
+def paint_phi():
     for i in range(NF):
         a = vertices[faces[i].x]
         b = vertices[faces[i].y]
         c = vertices[faces[i].z]
-        k = phi[i] * 0.005
+        k = phi[i] * 0.002
         color = k * tl.D.xyy + (1 - k) * tl.D.yxy
         try:
             gui.triangle(a, b, c, color=ti.rgb_to_hex(color))
@@ -58,7 +91,7 @@ def paint_P():
             gui.triangle(a, b, c)
 
 
-def pull(m0, m1):
+def pull(m0):
     closest = -1
     closest_dist = 1e5
     for i in range(NV):
@@ -66,9 +99,7 @@ def pull(m0, m1):
         if dist < closest_dist:
             closest, closest_dist = i, dist
 
-    vertices[closest] = m1.entries
-    #vertices[closest].x = vertices[closest].x + dm.x
-    #vertices[closest].y = vertices[closest].y + dm.y
+    vertices[closest] = m0.entries
 
 
 @ti.kernel
@@ -77,10 +108,8 @@ def init():
         faces[i] = [i, i + 1, i + 2]
     for i in range(NV):
         vertices[i] = tl.vec(i / NV * 0.8 + 0.1,
-                0.5 + ti.pow(-1, i) * (ti.random() * 0.2 + 0.9) * 0.15)
+                0.5 + ti.pow(-1, i) * (ti.random() * 0.0 + 0.9) * 0.15)
 
-
-m0 = None
 
 init()
 update_B()
@@ -90,17 +119,15 @@ while gui.running:
         if e.key == gui.ESCAPE:
             gui.running = False
         elif e.key == gui.LMB:
-            if e.type == gui.PRESS:
-                if m0 is None:
-                    m0 = tl.vec(*gui.get_cursor_pos())
-            else:
-                m1 = tl.vec(*gui.get_cursor_pos())
-                pull(m0, m1)
-                m0 = None
+            pull(tl.vec(*gui.get_cursor_pos()))
+            m0 = None
 
+    dF.fill(0)
     update_F()
     update_P()
-    paint_P()
+    advance()
+    update_phi()
+    paint_phi()
 
     gui.circles(vertices.to_numpy(), radius=4)
     gui.show()
